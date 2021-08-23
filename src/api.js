d2b311a69bd7cc36f9657d01d2e2c8af2b5375d4
Daniel Chistyakov <47912489+DCStudioProjects@users.noreply.github.com>
2021-08-23 14:52:07 +0300

    Debugging

--- src/api.js
+++ src/api.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const serverless = require("serverless-http");
-
+const axios = require("axios");
 const app = express();
 const router = express.Router();
+const querystring = require("querystring");
 
 router.get("/", (req, res) => {
+  const { data } = await axios.post(
+    `http://rezkance.com/ajax/get_cdn_series/`,
+    querystring.stringify(
+      {
+        id: 1154,
+        translator_id: 8,
+        season: 1,
+        episode: 1,
+        action: "get_stream",
+      }
+    )
+  );
   res.json({
-    hello: "hi!"
+    data
   });
 });
 
